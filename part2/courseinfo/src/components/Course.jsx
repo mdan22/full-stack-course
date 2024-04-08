7004c2590d8a5d81cf7c7876f9e70294c0befe8d
Daniel Kuhn <daniel.kuhn.dev@gmail.com>
2024-04-08 12:44:31 +0200

    add solution for 2.3*

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -6,27 +6,36 @@ import Total from "./Total"
  * It calculates the Sum of property p
  * for each element of array a using
  * a for-loop.
- * This might be used again - so it's
- * defined in the Course component.
- * @param {*} a
- * @param {*} p 
+ * @param {*} array
+ * @param {*} property
  */
-const calculateSum = (a, p) =>
-{
-    let sum = 0
-    for (let i = 0; i < a.length; i++) {
-        console.log(a[i][p]);
-        sum += a[i][p];
-        // a[i].p doesn't work
-    }
-    return sum
+// const sum = (a, p) =>
+// {
+//     let sum = 0
+//     for (let i = 0; i < a.length; i++) {
+//         console.log(a[i][p]);
+//         sum += a[i][p];
+//         // a[i].p doesn't work
+//     }
+//     return sum
+// }
+
+/**
+ * It calculates the Sum of the values
+ * of a certain property for each
+ * element of array a using reduce.
+ * @param {*} array
+ * @param {*} property
+ */
+const sum = (array, property) => {
+    return array.reduce((sum, element) => sum + element[property], 0);
 }
 
 const Course = ({course}) =>
   <>
     <Header course={course.name}/>
     <Content parts={course.parts}/>
-    <Total sum={calculateSum(course.parts,'exercises')} />
+    <Total sum={sum(course.parts,'exercises')} />
   </>
 
 export default Course
